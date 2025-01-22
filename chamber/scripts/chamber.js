c08592d824bf264402396b3939b29914774b4412
Jordan C <coker.jordan@gmail.com>
2025-01-21 22:23:24 -0500

    updates

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -80,3 +80,13 @@ async function getMembers() {
     setGrid.className=""
     cards.className='list'
   })
+
+  // Display the last modified date for the footer
+const lastModifiedElement = document.getElementById("lastModified");
+const currentYearElement = document.getElementById("year");
+
+const currentYear = new Date().getFullYear();
+currentYearElement.textContent = `© ${currentYear} Jordan Coker - Michigan, USA`;
+
+const lastModifiedDate = document.lastModified;
+lastModifiedElement.textContent = `Last Update: ${lastModifiedDate}`;
