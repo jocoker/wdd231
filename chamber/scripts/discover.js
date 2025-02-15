cb9efb109bc5d41e2cca87f2329e6dc873800921
Jordan C <coker.jordan@gmail.com>
2025-02-15 17:03:43 -0500

    updatedd

---
+++ chamber/scripts/discover.js
@@ -0,0 +1,38 @@
+//---------- GRAB A REFERENCE TO THE DIVISION WHERE WE DISPLAY THE ITEMS 
+const showHere = document.querySelector("#places")
+
+
+// ---------------- LOOP THROUGH THE ARRAY OF JSON ITEMS 
+function displayItems(places) {
+  places.forEach(x => {
+    //build the card element
+    const thecard = document.createElement('div')
+    //build the photo element
+    const thephoto = document.createElement('img')
+    thephoto.src = `images/${x.photo_link}`
+    thephoto.alt = x.name
+    thecard.appendChild(thephoto)
+    //build the title element
+    const thetitle = document.createElement('h2')
+    thetitle.innerText = x.name
+    thecard.appendChild(thetitle)
+    //build the address element
+    const theaddress = document.createElement('address')
+    theaddress.innerText = x.address
+    thecard.appendChild(theaddress)
+    //build the description element
+    const thedesc = document.createElement('p')
+    thedesc.innerHTML = `${x.description} <br><br>COST:  ${x.cost}`
+    thecard.appendChild(thedesc)
+    //build the button
+    const thelearn = document.createElement('button')
+    thelearn.innerText = "Learn More"
+    thecard.appendChild(thelearn)
+
+
+    showHere.appendChild(thecard)
+  }) // end loop
+} // end function
+
+// START DISPLAYING ALL ITEMS IN THE JSON FILE
+displayItems(places)
